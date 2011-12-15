e55c7f1bca1748d8f986afca571cc0629e7dc3ae
Antoine Grant <antoinegrant@gmail.com>
2011-12-15 00:55:48 -0800

    revert

--- v1.rb
+++ v1.rb
@@ -1,26 +1,6 @@
 require 'rubygems'
 require 'sinatra'
-require 'sinatra/activerecord'
-
-#set :database, 'mysql://agrantmysql1:Climbing111@mysq.drtopo.co/drtopov4'
-
-ActiveRecord::Base.establish_connection(
-  :adapter  => 'mysql2',
-  :socket   => '/tmp/mysql.sock',
-  :username => 'agrantmysql1',
-  :password => 'Climbing111',
-  :database => 'drtopov4',
-  :encoding => 'utf8'
-)
 
 get '/' do
   "Hello World!!!"
 end
-
-__END__
-
-NOTES
-------------------------------
-http://screencasts.org/episodes/configuring-activerecord-in-sinatra
-http://devcenter.heroku.com/articles/rack
-https://github.com/bmizerany/sinatra-activerecord
