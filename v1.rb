c77b8541d3a9bce030ea9d689b36a738d227aecc
Antoine Grant <antoinegrant@gmail.com>
2011-12-15 22:58:39 -0800

    a

--- v1.rb
+++ v1.rb
@@ -2,14 +2,14 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/activerecord'
 
-ActiveRecord::Base.establish_connection(
-  :adapter  => 'mysql2',
-  :socket   => '/tmp/mysql.sock',
-  :username => 'agrantmysql1',
-  :password => 'Climbing111',
-  :database => 'drtopov4',
-  :encoding => 'utf8'
-)
+# ActiveRecord::Base.establish_connection(
+#   :adapter  => 'mysql2',
+#   :socket   => '/tmp/mysql.sock',
+#   :username => 'agrantmysql1',
+#   :password => 'Climbing111',
+#   :database => 'drtopov4',
+#   :encoding => 'utf8'
+# )
 
 environment = (ENV['RACK_ENV'] ? ENV['RACK_ENV'].to_sym : :development)
 
