659815229a07387b60ec45d1dfdcfeb0c7712efa
Antoine Grant <antoinegrant@gmail.com>
2011-12-17 23:47:53 -0800

    Try

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'active_record'
+
+@environment = (ENV['RACK_ENV'] ? ENV['RACK_ENV'].to_sym : :development)
+
+db_config = YAML::load(IO.read('config/database.yml'))["#{@environment}"]
+ActiveRecord::Base.establish_connection(
+  :adapter  => db_config['adapter'],
+  :host     => db_config['host'],
+  :socket   => db_config['socket'],
+  :username => db_config['username'],
+  :password => db_config['password'],
+  :database => db_config['database'],
+  :encoding => db_config['encoding']
+)
--- v1.rb
+++ v1.rb
@@ -1,28 +1,12 @@
-require 'sinatra'
-require 'active_record'
-
-environment = (ENV['RACK_ENV'] ? ENV['RACK_ENV'].to_sym : :development)
-
-db_config = YAML::load(IO.read('config/database.yml'))["#{environment}"]
-ActiveRecord::Base.establish_connection(
-  :adapter  => db_config['adapter'],
-  :host     => db_config['host'],
-  :socket   => db_config['socket'],
-  :username => db_config['username'],
-  :password => db_config['password'],
-  :database => db_config['database'],
-  :encoding => db_config['encoding']
-)
-
-class Products < ActiveRecord::Base
-end
+require './config/application'
+require './models/product'
 
 before do
   ActiveRecord::Base.connection.verify!
 end
 
 get '/hello' do
-  "Hello world! #{environment}"
-  product = Products.find(:all).first
+  "Hello world! #{@environment}"
+  product = Product.find(:all).first
   "#{product.to_json}"
 end
