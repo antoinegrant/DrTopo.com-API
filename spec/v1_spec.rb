900f80d895e3719dae1b80a0818c7ce9ac7b2943
Antoine Grant <antoinegrant@gmail.com>
2011-12-20 18:17:51 -0800

    Test

--- spec/v1_spec.rb
+++ spec/v1_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
-describe 'The API should, ' do
-  it 'get the home page' do
-    get '/'
-    last_response.status.should == 200
+describe 'The API should,' do
+  
+  context 'start and' do
+    
+    it 'get the home page' do
+      get '/'
+      last_response.status.should == 200
+    end
+    
+    it 'be in test mode' do
+      get '/env'
+      JSON.parse(last_response.body)['env'].should == 'test'
+    end
+    
   end
-  it 'be in test mode' do
-    get '/env'
-    JSON.parse(last_response.body)['env'].should == 'test'
-  end
+  
 end
